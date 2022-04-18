5a0bb0665761f7bce7e4e3e77f7e6980314b78ba
Alex Pletzer <alexander@gokliya.net>
2022-04-19 11:16:33 +1200

    debugging

--- R/build_model_run.R
+++ R/build_model_run.R
@@ -78,7 +78,7 @@ build_model_run <- function(project_path, run_path, n_thread, os, swat_vers, qui
       .[grepl(".exe$",.)]
 
   } else if(os == "unix") {
-    swat_exe <- system("find"%&&%project_path%&&%"-type f -perm -111",
+    swat_exe <- system("find"%&&%project_path%&&%"-depth 1 -type f -perm -111",
                         intern = T) %>%
       basename(.)
   }
